7522aa4d02853ae37c84099b3e8cd85f82fb826a
Brian Adams <brianadams@sevenhillstechnology.com>
2021-08-28 23:00:17 -0400

    Update RecordScreen.h

--- ios/RecordScreen.h
+++ ios/RecordScreen.h
@@ -11,6 +11,7 @@
     @property (assign, nonatomic) int screenWidth;
     @property (assign, nonatomic) int screenHeight;
     @property (assign, nonatomic) BOOL enableMic;
+    @property (strong, nonatomic) BOOL micDisabled;
 
     @property (nonatomic) AVAssetWriter *writer;
     @property BOOL encounteredFirstBuffer;
